bd5dd1b0f0cedcd0d4969cfbb54bef7f3b967293
Sebastian Lugo <sebalugo@gmail.com>
2016-08-21 16:59:42 -0400

    Adding thunk action creators

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -7,22 +7,88 @@ export const ADD_SCORE = 'ADD_SCORE';
 export const MOVE_TETROMINO = 'MOVE_TETROMINO';
 export const ADD_TETROMINO = 'ADD_TETROMINO';
 
+function checkCollisions(direction, activeTetrominos, currentTetromino) {
+	switch (direction) {
+	case 'left':
+		break;
+	case 'right':
+		break;
+	case 'up':
+		break;
+	case 'down':
+		break;
+	default:
+		return true;
+	}
+	return true;
+}
+
+export const rotateTetromino = (orientation) => {
+	return function (dispatch, getState){
+		const { activeTetrominos } = getState();
+	};
+};
+
+export const moveTetromino = (direction) => {
+	return function(dispatch, getState){
+		const { activeTetrominos, currentTetromino } = getState();
+		switch (direction) {
+		case 'left':
+			if (checkCollisions(direction, activeTetrominos, currentTetromino)) {
+				//dispatch new tetromino with new position
+			}
+			return;
+		case 'right':
+			if (checkCollisions(direction, activeTetrominos, currentTetromino)) {
+				//dispatch new tetromino with new position
+			}
+			return;
+		default:
+			return;
+		}
+	};
+};
+
+export const loadGame = () => {
+
+	return function(dispatch){
+		dispatch(startGame());
+
+		function handleMoving(e) {
+			e.preventDefault();
+			switch (e.keyCode){
+			case 37:
+				dispatch(moveTetromino('left'));
+				console.log("LEFT");
+				break;
+			case 39:
+				dispatch(moveTetromino('right'));
+				console.log("RIGHT");
+				break;
+			}
+		}
+		function handleRotation(e) {
+			switch (e.keyCode){
+			case 38:
+				console.log("UP");
+				break;
+			case 40:
+				console.log("DOWN");
+				break;
+			}
+		}
+		window.addEventListener('keydown', handleMoving);
+		window.addEventListener('keydown', handleRotation);
+	};
+};
+
 export const spawnTetromino = (tetrominoType, offsetX, offsetY) => ({
 	type: SPAWN_TETROMINO,
 	tetrominoType,
 	offsetX,
 	offsetY,
 });
-export const rotateTetromino = (id, orientation) => ({
-	type: ROTATE_TETROMINO,
-	id,
-	orientation,
-});
-export const moveTetromino = (id, direction) => ({
-	type: MOVE_TETROMINO,
-	id,
-	direction,
-});
+
 export const addTetromino = (position) => ({
 	type: ADD_TETROMINO,
 	position,
