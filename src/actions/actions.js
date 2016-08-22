d4cf94aedce6f72ceee68ae9d3f90d40dc0fe73d
Sebastian Lugo <sebalugo@gmail.com>
2016-08-22 19:41:50 -0400

    Finishing shape rotation, implementing adding shape to field

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,3 +1,4 @@
+
 import gameConstants from '../gameConstants.js';
 
 export const SPAWN_TETROMINO = 'SPAWN_TETROMINO';
@@ -12,47 +13,39 @@ export const MOVE_LEFT = 'MOVE_LEFT';
 export const MOVE_DOWN = 'MOVE_DOWN';
 export const ADD_TETROMINO = 'ADD_TETROMINO';
 
-export const spawnTetromino = (tetrominoType, offsetX, offsetY) => ({
-	type: SPAWN_TETROMINO,
-	tetrominoType,
-	offsetX,
-	offsetY,
-});
+function getActualCoordinates(newTetromino) {
+	const coordinates = [];
+	const { shape, offsetX, offsetY } = newTetromino;
+	const { blockUnit } = gameConstants;
+	for (let i = 0; i < shape.length; i++) {
+		for (let j = 0; j < shape[i].length; j++) {
+			if (shape[i][j]) {
+				coordinates.push({ x: j + (offsetX / blockUnit), y: i + (offsetY / blockUnit) });
+			}
+		}
+	}
+	return coordinates;
+}
 
-export const addTetromino = (position) => ({
-	type: ADD_TETROMINO,
-	position,
-});
-export const startGame = () => ({
-	type: START_GAME,
-});
-export const stopGame = () => ({
-	type: STOP_GAME,
-});
-export const clearLine = (position) => ({
-	type: CLEAR_LINE,
-	position,
-});
-export const addScore = (points) => ({
-	type: ADD_SCORE,
-	points,
-});
+function rotateArray(matrix) {
+	const n = matrix.length;
+	const ret = [[], [], [], []];
+	let closestX = 10;
 
-export const moveRight = () => ({
-	type: MOVE_RIGHT,
-});
-
-export const moveLeft = () => ({
-	type: MOVE_LEFT,
-});
-
-export const moveDown = () => ({
-	type: MOVE_DOWN,
-});
-
-export const rotateRight = () => ({
-	type: ROTATE_TETROMINO,
-});
+	for (let i = 0; i < n; ++i) {
+		for (let j = 0; j < n; ++j) {
+			ret[i][j] = matrix[n - j - 1][i];
+			if (ret[i][j]) {
+				closestX = Math.min(closestX, j);
+			}
+		}
+	}
+	const fill = new Array(closestX).fill(0);
+	for (let i = 0; i < n; ++i) {
+		ret[i] = ret[i].slice(closestX).concat(fill);
+	}
+	return ret;
+}
 
 function checkCollisions(direction, activeTetrominos, currentTetromino) {
 	const { blockUnit, fieldWidth, fieldHeight } = gameConstants;
@@ -95,16 +88,81 @@ function checkCollisions(direction, activeTetrominos, currentTetromino) {
 	return true;
 }
 
+export const spawnTetromino = (tetrominoType, offsetX, offsetY) => ({
+	type: SPAWN_TETROMINO,
+	tetrominoType,
+	offsetX,
+	offsetY,
+});
+
+export const addTetromino = (currentTetromino, nextTetromino) => {
+	const { shapesMapping } = gameConstants;
+	const newRandomNumber = Math.floor(Math.random() * 7);
+	const nextRandomShape = shapesMapping[newRandomNumber];
+
+	return {
+		type: ADD_TETROMINO,
+		currentTetromino: getActualCoordinates(currentTetromino),
+		color: currentTetromino.color,
+		nextTetromino,
+		nextRandomShape,
+	};
+};
+
+export const startGame = () => {
+	const { shapesMapping } = gameConstants;
+	const currentRandomNumber = Math.floor(Math.random() * 7);
+	const nextRandomNumber = Math.floor(Math.random() * 7);
+	const currentRandomShape = shapesMapping[currentRandomNumber];
+	const nextRandomShape = shapesMapping[nextRandomNumber];
+
+	return {
+		type: START_GAME,
+		currentRandomShape,
+		nextRandomShape,
+	};
+};
+export const stopGame = () => ({
+	type: STOP_GAME,
+});
+export const clearLine = (position) => ({
+	type: CLEAR_LINE,
+	position,
+});
+export const addScore = (points) => ({
+	type: ADD_SCORE,
+	points,
+});
+
+export const moveRight = () => ({
+	type: MOVE_RIGHT,
+});
+
+export const moveLeft = () => ({
+	type: MOVE_LEFT,
+});
+
+export const moveDown = () => ({
+	type: MOVE_DOWN,
+});
+
+export const rotateRight = (tetromino) => ({
+	type: ROTATE_TETROMINO,
+	rotatedTetromino: rotateArray(tetromino),
+});
+
 export const rotateTetromino = () => (
 	function (dispatch, getState) {
 		const { activeTetrominos, currentTetromino } = getState();
-			dispatch(rotateRight());
+		if (!checkCollisions('rotation', activeTetrominos, currentTetromino)) {
+			dispatch(rotateRight(currentTetromino.shape));
+		}
 	}
 );
 
 export const moveTetromino = (direction) => (
 	function (dispatch, getState) {
-		const { activeTetrominos, currentTetromino } = getState();
+		const { activeTetrominos, currentTetromino, nextTetromino } = getState();
 		switch (direction) {
 		case 'left':
 			if (checkCollisions(direction, activeTetrominos, currentTetromino)) {
@@ -119,6 +177,8 @@ export const moveTetromino = (direction) => (
 		case 'down':
 			if (checkCollisions(direction, activeTetrominos, currentTetromino)) {
 				dispatch(moveDown());
+			} else {
+				dispatch(addTetromino(currentTetromino, nextTetromino));
 			}
 			return;
 		default:
@@ -130,17 +190,18 @@ export const moveTetromino = (direction) => (
 export const loadGame = () => (
 	function (dispatch) {
 		dispatch(startGame());
-
 		function handleMoving(e) {
-			e.preventDefault();
 			switch (e.keyCode) {
 			case 37:
+				e.preventDefault();
 				dispatch(moveTetromino('left'));
 				break;
 			case 39:
+				e.preventDefault();
 				dispatch(moveTetromino('right'));
 				break;
 			case 40:
+				e.preventDefault();
 				dispatch(moveTetromino('down'));
 				break;
 			default:
@@ -150,6 +211,7 @@ export const loadGame = () => (
 		function handleRotation(e) {
 			switch (e.keyCode) {
 			case 38:
+				e.preventDefault();
 				dispatch(rotateTetromino());
 				break;
 			default:
@@ -162,3 +224,6 @@ export const loadGame = () => (
 		window.addEventListener('keydown', handleRotation);
 	}
 );
+
+
+
