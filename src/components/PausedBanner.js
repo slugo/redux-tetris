ed1100d5b9abca8113cfa5f74b8066e90fba2dc6
Sebastian Lugo <sebalugo@gmail.com>
2016-09-06 20:27:38 -0400

    Adding PausedBanner component

---
+++ src/components/PausedBanner.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import style from '../styles/styles.css';
+
+const PausedBanner = () => (
+	<div className={style.pausedBanner}>
+		<h1 style={{ paddingTop: '250px' }}>PAUSED</h1>
+	</div>
+);
+
+export default PausedBanner;
+
+
+
